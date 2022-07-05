e5585ea365da95428cd38347ce530dc564e8ecfd
“julie <chouyuan424@gamil.com>
2022-07-05 19:35:07 +0800

    feat: modify routes

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.set('view engine', 'hbs')
 
 // routes setting 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { title: "首頁" });
 })
-
+app.get('/:subPage', (req, res) => {
+  res.render('index', { title: req.params.subPage });
+})
 // start and listen on the app
 app.listen(3000, () => {
   console.log('Express is listening on http://localhost:3000')
