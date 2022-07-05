65d29ca45e0cd80b60ab62c18c3cd37b38648f4e
“julie <chouyuan424@gamil.com>
2022-07-05 21:57:32 +0800

    feat: adding styles and adjusting layout

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
+app.use(express.static('public'))
+
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
